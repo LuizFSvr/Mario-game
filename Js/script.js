734fff822298a05c8cc3908bad202ab4e2a9ba3e
Luiz Felipe Gomes Vieira <luizpkm2016@gmail.com>
2023-04-27 00:10:58 -0300

    Project mario initial develop

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+const mario = document.querySelector('.mario');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+window.addEventListener('keydown', jump);
